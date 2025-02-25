5c94e7f08036bc83ea2f674ee961dc415fc611ac
phkuzyanin <phkuzyanin@gmail.com>
2025-02-25 21:16:46 +0300

    commit

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+app.Use(Configure);
 
-app.MapGet("/", () => "Hello World!");;
 
 app.Run();
+async Task Configure(HttpContext context, Func<Task> next){
+    context.Response.ContentType = "text/plain; charset=utf-8";
+    await context.Response.WriteAsync("Сработал  Use");
+}
