5f6b7813198b145da0f32a770809b59d60e64740
phkuzyanin <phkuzyanin@gmail.com>
2025-03-01 23:28:49 +0300

    Read.me and IUserService

---
+++ Sevices/ICardService.cs
@@ -0,0 +1,15 @@
+using System;
+using Models.List;
+using Models.User;
+
+namespace Services.ICardService;
+
+interface ICardService{
+    Task<ListCard> CreateCard();
+    Task<ListCard> GetListByBoardId();
+    Task<ListCard> UpdateList();
+    Task<ListCard>DeleteCard();
+    Task<ListCard> AddLabelToCard();
+    Task<ListCard> MoveCard();
+    Task<User> AddCommentToCard();
+}
--- obj/Debug/net9.0/TaskManager.AssemblyInfo.cs
+++ obj/Debug/net9.0/TaskManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0043fad9520d4ee9916c791421d798a569160f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+529b7808180e64614bf19aa8a7bf4db85ed7550a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
